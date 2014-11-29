5522a7f5085ede0792dccb4f421e93544536fb4c
Sai Sriparasa <sai.sriparasa@yahoo.com>
2014-11-29 16:48:14 -0500

    Pushing the basic files

---
+++ src/main/java/com/java_mvn_sparkstreaming_window/SparkStreamingS3/App.java
@@ -0,0 +1,13 @@
+package com.java_mvn_sparkstreaming_window.SparkStreamingS3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
